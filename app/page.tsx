d88f64877daea69c8aaa854510e7e95399a76d10
Tungetyt <static0sphere@gmail.com>
2024-08-04 16:51:22 +0200

    feat: better layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TextareaHighlight from '@/components/textarea-highlight'
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+		<main className='flex min-h-screen justify-between p-4'>
 			<TextareaHighlight />
 			<ModeToggle />
 		</main>
